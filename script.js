4cfeb7e77c83372fc42f5eda5f0e38395d3e0fa2
RakshitaPatil28 <patilrakshita28@gmail.com>
2026-03-10 19:59:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-let seconds=0;
-let minutes=0;
-let hours=0;
-
-let stopwatchInterval;
-
 function addUser(){
 
 let name=document.getElementById("username").value;
@@ -16,83 +10,48 @@ return;
 let userDiv=document.createElement("div");
 userDiv.className="userBox";
 
-let userTitle=document.createElement("h3");
-userTitle.innerText=name;
+let title=document.createElement("h3");
+title.innerText=name;
 
-let deleteUser=document.createElement("button");
-deleteUser.innerText="Delete User";
+/* STOPWATCH */
+
+let swDisplay=document.createElement("h2");
+swDisplay.innerText="00:00:00";
+
+let startBtn=document.createElement("button");
+startBtn.innerText="Start";
+
+let stopBtn=document.createElement("button");
+stopBtn.innerText="Stop";
+
+let resetBtn=document.createElement("button");
+resetBtn.innerText="Reset";
+
+/* TIMER */
 
 let timerInput=document.createElement("input");
 timerInput.placeholder="Minutes";
 
-let addTimerBtn=document.createElement("button");
-addTimerBtn.innerText="Add Timer";
+let timerDisplay=document.createElement("h3");
+timerDisplay.innerText="00:00";
 
-let timerArea=document.createElement("div");
-
-userDiv.appendChild(userTitle);
-userDiv.appendChild(deleteUser);
-userDiv.appendChild(document.createElement("br"));
-userDiv.appendChild(timerInput);
-userDiv.appendChild(addTimerBtn);
-userDiv.appendChild(timerArea);
-
-document.getElementById("userList").appendChild(userDiv);
-
-document.getElementById("username").value="";
-
-deleteUser.onclick=function(){
-userDiv.remove();
-}
-
-addTimerBtn.onclick=function(){
-
-let min=timerInput.value;
-
-if(min===""){
-alert("Enter minutes");
-return;
-}
-
-let total=min*60;
-
-let timerDiv=document.createElement("div");
-
-let timeDisplay=document.createElement("p");
+let timerBtn=document.createElement("button");
+timerBtn.innerText="Start Timer";
 
 let deleteTimerBtn=document.createElement("button");
 deleteTimerBtn.innerText="Delete Timer";
 
-timerDiv.appendChild(timeDisplay);
-timerDiv.appendChild(deleteTimerBtn);
+/* DELETE USER */
 
-timerArea.appendChild(timerDiv);
+let deleteUser=document.createElement("button");
+deleteUser.innerText="Delete User";
 
-let timer=setInterval(function(){
+/* STOPWATCH VARIABLES */
 
-let m=Math.floor(total/60);
-let s=total%60;
-
-timeDisplay.innerText=
-`${String(m).padStart(2,'0')}:${String(s).padStart(2,'0')}`;
-
-total--;
-
-if(total<0){
-clearInterval(timer);
-timeDisplay.innerText="Finished!";
-}
-
-},1000);
-
-deleteTimerBtn.onclick=function(){
-clearInterval(timer);
-timerDiv.remove();
-}
-
-}
-
-}
+let seconds=0;
+let minutes=0;
+let hours=0;
+let swInterval;
 
 function updateStopwatch(){
 
@@ -108,27 +67,95 @@ minutes=0;
 hours++;
 }
 
-document.getElementById("stopwatch").innerText=
+swDisplay.innerText =
 `${String(hours).padStart(2,'0')}:${String(minutes).padStart(2,'0')}:${String(seconds).padStart(2,'0')}`;
 
 }
 
-function startStopwatch(){
-stopwatchInterval=setInterval(updateStopwatch,1000);
+startBtn.onclick=function(){
+swInterval=setInterval(updateStopwatch,1000);
 }
 
-function stopStopwatch(){
-clearInterval(stopwatchInterval);
+stopBtn.onclick=function(){
+clearInterval(swInterval);
 }
 
-function resetStopwatch(){
-
-clearInterval(stopwatchInterval);
-
+resetBtn.onclick=function(){
+clearInterval(swInterval);
 seconds=0;
 minutes=0;
 hours=0;
+swDisplay.innerText="00:00:00";
+}
 
-document.getElementById("stopwatch").innerText="00:00:00";
+/* TIMER */
+
+let timerInterval;
+
+timerBtn.onclick=function(){
+
+let total=timerInput.value*60;
+
+if(total<=0){
+alert("Enter minutes");
+return;
+}
+
+timerInterval=setInterval(function(){
+
+let m=Math.floor(total/60);
+let s=total%60;
+
+timerDisplay.innerText=
+`${String(m).padStart(2,'0')}:${String(s).padStart(2,'0')}`;
+
+total--;
+
+if(total<0){
+clearInterval(timerInterval);
+timerDisplay.innerText="Finished!";
+}
+
+},1000);
+
+}
+
+deleteTimerBtn.onclick=function(){
+clearInterval(timerInterval);
+timerDisplay.innerText="00:00";
+timerInput.value="";
+}
+
+/* DELETE USER */
+
+deleteUser.onclick=function(){
+clearInterval(swInterval);
+clearInterval(timerInterval);
+userDiv.remove();
+}
+
+/* ADD ELEMENTS */
+
+userDiv.appendChild(title);
+
+userDiv.appendChild(swDisplay);
+userDiv.appendChild(startBtn);
+userDiv.appendChild(stopBtn);
+userDiv.appendChild(resetBtn);
+
+userDiv.appendChild(document.createElement("hr"));
+
+userDiv.appendChild(timerInput);
+userDiv.appendChild(timerDisplay);
+userDiv.appendChild(timerBtn);
+userDiv.appendChild(deleteTimerBtn);
+
+userDiv.appendChild(document.createElement("br"));
+
+userDiv.appendChild(deleteUser);
+
+document.getElementById("userList").appendChild(userDiv);
+
+document.getElementById("username").value="";
 
 }
